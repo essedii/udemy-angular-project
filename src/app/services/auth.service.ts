0595a49c07153a8434237a6d061c0523e23e7b8e
Stefano Dolci <ste.dolci16@gmail.com>
2023-11-14 20:24:27 +0100

    Create is logged behavior subject

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  private isLoggedInSubject: BehaviorSubject(false) = new BehaviorSubject<boolean>(false)
   signIn(email: string, password: string): void {
     console.log(email, password)
   }
